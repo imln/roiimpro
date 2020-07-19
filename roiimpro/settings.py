79c468d68e7481843b6eb386f7a86aa87fa3fd0c
LNGupta <campustray@gmail.com>
2020-07-19 20:44:32 +0530

    roiimproj added

--- roiimpro/settings.py
+++ roiimpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8lcixb_e#xx6=_k+m0dk1=@qmx+47^we8!l67hs1curf+q8=ps'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://roiimpro.herokuapp.com/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['https://roiimpro.herokuapp.com/','https://roiimproj.herokuapp.com/','127.0.0.1','localhost']
 
 
 # Application definition
